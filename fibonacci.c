1fc25c1f406d2f5a19815072ffb370ff7684f7bd
arunpandey77 <34696977+arunpandey77@users.noreply.github.com>
2025-11-25 11:18:44 +0530

    Add files via upload

---
+++ fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int fibonacci(int n) {
+    if (n <= 0) return 0;
+    if (n == 1) return 1;
+
+    int a = 0, b = 1, c = 0;
+    for (int i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+
+int main(void) {
+    int n = 10;
+
+    printf("Fibonacci series up to %d terms:\n", n);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", fibonacci(i));
+    }
+    printf("\n");
+    return 0;
+}
