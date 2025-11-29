8de07344f1d3b81f8f09026e4961dd070c1d8254
arunpandey77 <34696977+arunpandey77@users.noreply.github.com>
2025-11-29 23:13:20 +0530

    cotlin_code

---
+++ sum_array.kt
@@ -0,0 +1,15 @@
+fun sumArray(arr: IntArray, length: Int): Int {
+    var sum = 0
+    for (i in 0 until length) {
+        sum += arr[i]
+    }
+    return sum
+}
+
+fun main() {
+    val numbers = intArrayOf(1, 2, 3, 4, 5)
+    val length = numbers.size
+
+    val result = sumArray(numbers, length)
+    println("Sum of array = $result")
+}
