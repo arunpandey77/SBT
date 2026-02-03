f1cb79ac4e7f7242d994355f1bd1fd2c902de8ea
arunpandey77 <34696977+arunpandey77@users.noreply.github.com>
2026-02-03 07:24:41 +0530

    cotlin_code

--- fibonacci.kt
+++ fibonacci.kt
@@ -1,7 +1,6 @@
 fun fibonacci(n: Int): Int {
     if (n <= 0) return 0
     if (n == 1) return 1
-
     var a = 0
     var b = 1
     var c: Int
@@ -15,7 +14,8 @@ fun fibonacci(n: Int): Int {
 
 fun main() {
     val n = 10
-    println("Fibonacci series up to $n terms:")
+    print("Fibonacci series up to $n terms:
+")
     for (i in 0 until n) {
         print("${fibonacci(i)} ")
     }
