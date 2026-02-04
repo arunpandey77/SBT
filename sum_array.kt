4ef58ef54a6d5feb3d15e5350cd2c523a233155c
arunpandey77 <34696977+arunpandey77@users.noreply.github.com>
2026-02-04 16:42:51 +0530

    cotlin_code

--- sum_array.kt
+++ sum_array.kt
@@ -1,13 +1,14 @@
 fun sumArray(arr: IntArray): Int {
     var sum = 0
-    for (value in arr) {
-        sum += value
+    for (num in arr) {
+        sum += num
     }
     return sum
 }
 
 fun main() {
     val numbers = intArrayOf(1, 2, 3, 4, 5)
+
     val result = sumArray(numbers)
     println("Sum of array = $result")
 }
